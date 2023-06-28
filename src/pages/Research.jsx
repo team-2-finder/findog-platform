43ad7c94eda38e839732f5dd4c0ba1567ba90f5b
YimKeul <leesjun29@gmail.com>
2023-06-29 03:38:52 +0900

    feat : add loading state

--- src/pages/Research.jsx
+++ src/pages/Research.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import styled from "styled-components";
+import { InputImage, Header } from "../components";
 
 const Research = () => {
   return (
-    <div>
+    <>
+      <Header />
       <p>Research page</p>
-    </div>
+    </>
   );
 };
 
