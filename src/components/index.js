e03caf708f31a6121a564f0254b3a2ac9038ceef
YimKeul <leesjun29@gmail.com>
2023-06-29 05:15:17 +0900

    Remove file

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
-import InputImage from "./InputImage";
 import Header from "./Header";
 import Loading from "./Loading";
 import { MainColor, DisableMain } from "./Colors";
-export { InputImage, Header, MainColor, DisableMain, Loading };
+export { Header, MainColor, DisableMain, Loading };
